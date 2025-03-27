42855a984d4b98e478a47f1f70ed29fed1f27aba
Rakibul <rakibuliuict@gmail.com>
2025-03-27 15:25:25 +0600

    resnet3d.py

--- PICAI_SSL/networks/resnet3d.py
+++ PICAI_SSL/networks/resnet3d.py
@@ -1,12 +1,9 @@
-import math
 import torch
 import torch.nn as nn
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
            'resnet18_d', 'resnet34_d', 'resnet50_d', 'resnet101_d', 'resnet152_d',
            'resnet50_16s', 'resnet50_w2x', 'resnext101_32x8d', 'resnext152_32x8d']
-
-
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -17,7 +14,7 @@ def conv3x3_bn_relu(in_planes, out_planes, stride=1):
     return nn.Sequential(
         conv3x3(in_planes, out_planes, stride),
         nn.InstanceNorm3d(out_planes),
-        nn.ReLU()
+        nn.ReLU(inplace=True)
     )
 
 
@@ -32,10 +29,9 @@ class BasicBlock(nn.Module):
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.InstanceNorm3d(planes)
         self.downsample = downsample
-        self.stride = stride
 
     def forward(self, x):
-        residual = x
+        identity = x
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -45,11 +41,10 @@ class BasicBlock(nn.Module):
         out = self.bn2(out)
 
         if self.downsample is not None:
-            residual = self.downsample(x)
+            identity = self.downsample(x)
 
-        out += residual
+        out += identity
         out = self.relu(out)
-
         return out
 
 
@@ -59,19 +54,22 @@ class Bottleneck(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1):
         super(Bottleneck, self).__init__()
         width = int(planes * (base_width / 64.)) * groups
+
         self.conv1 = nn.Conv3d(inplanes, width, kernel_size=1, bias=False)
         self.bn1 = nn.InstanceNorm3d(width)
+
         self.conv2 = nn.Conv3d(width, width, kernel_size=3, stride=stride, padding=dilation, dilation=dilation,
                                groups=groups, bias=False)
         self.bn2 = nn.InstanceNorm3d(width)
+
         self.conv3 = nn.Conv3d(width, planes * self.expansion, kernel_size=1, bias=False)
         self.bn3 = nn.InstanceNorm3d(planes * self.expansion)
+
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
-        self.stride = stride
 
     def forward(self, x):
-        residual = x
+        identity = x
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -85,25 +83,23 @@ class Bottleneck(nn.Module):
         out = self.bn3(out)
 
         if self.downsample is not None:
-            residual = self.downsample(x)
+            identity = self.downsample(x)
 
-        out += residual
+        out += identity
         out = self.relu(out)
-
         return out
 
 
 class ResNet(nn.Module):
-
     def __init__(self, block, layers, in_channel=3, width=1, groups=1, width_per_group=64,
-                 mid_dim=1024, low_dim=128, avg_down=False, deep_stem=False,
-                 head_type='mlp_head', layer4_dilation=1):
+                 avg_down=False, deep_stem=False, layer4_dilation=1):
         super(ResNet, self).__init__()
-        self.avg_down = avg_down
+
         self.inplanes = 16 * width
         self.base = int(16 * width)
         self.groups = groups
         self.base_width = width_per_group
+        self.avg_down = avg_down
 
         if deep_stem:
             self.conv1 = nn.Sequential(
@@ -118,17 +114,16 @@ class ResNet(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
 
-        self.layer1 = self._make_layer(block, self.base*2, layers[0], stride=2)
-        self.layer2 = self._make_layer(block, self.base*4, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, self.base*8, layers[2], stride=2)
+        self.layer1 = self._make_layer(block, self.base * 2, layers[0], stride=2)
+        self.layer2 = self._make_layer(block, self.base * 4, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, self.base * 8, layers[2], stride=2)
+
         if layer4_dilation == 1:
-            self.layer4 = self._make_layer(block, self.base*16, layers[3], stride=2)
+            self.layer4 = self._make_layer(block, self.base * 16, layers[3], stride=2)
         elif layer4_dilation == 2:
-            self.layer4 = self._make_layer(block, self.base*16, layers[3], stride=1, dilation=2)
+            self.layer4 = self._make_layer(block, self.base * 16, layers[3], stride=1, dilation=2)
         else:
             raise NotImplementedError
-
-        self.avgpool = nn.AvgPool3d(7, stride=1)
 
     def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
         downsample = None
@@ -137,50 +132,65 @@ class ResNet(nn.Module):
                 downsample = nn.Sequential(
                     nn.AvgPool3d(kernel_size=stride, stride=stride),
                     nn.Conv3d(self.inplanes, planes * block.expansion, kernel_size=1, stride=1, bias=False),
-                    nn.InstanceNorm3d(planes * block.expansion),
+                    nn.InstanceNorm3d(planes * block.expansion)
                 )
             else:
                 downsample = nn.Sequential(
                     nn.Conv3d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
-                    nn.InstanceNorm3d(planes * block.expansion),
+                    nn.InstanceNorm3d(planes * block.expansion)
                 )
 
         layers = [block(self.inplanes, planes, stride, downsample, self.groups, self.base_width, dilation)]
         self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
             layers.append(block(self.inplanes, planes, groups=self.groups, base_width=self.base_width, dilation=dilation))
-
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        c2 = self.layer1(x)
+
+        c1 = x
+        c2 = self.layer1(c1)
         c3 = self.layer2(c2)
         c4 = self.layer3(c3)
         c5 = self.layer4(c4)
-        return [x, c2, c3, c4, c5]
+
+        return [c1, c2, c3, c4, c5]
 
 
-# -----------------------------------------
-# ResNet 3D Architecture Factory Functions
-# -----------------------------------------
+# ------------------ FACTORY FUNCTIONS ------------------
 
 def resnet18(**kwargs):
     return ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
 
-def resnet18_d(**kwargs):
-    return ResNet(BasicBlock, [2, 2, 2, 2], deep_stem=True, avg_down=True, **kwargs)
-
 def resnet34(**kwargs):
     return ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+
+def resnet50(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+
+def resnet101(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+
+def resnet152(**kwargs):
+    return ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+
+def resnet18_d(**kwargs):
+    return ResNet(BasicBlock, [2, 2, 2, 2], deep_stem=True, avg_down=True, **kwargs)
 
 def resnet34_d(**kwargs):
     return ResNet(BasicBlock, [3, 4, 6, 3], deep_stem=True, avg_down=True, **kwargs)
 
-def resnet50(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+def resnet50_d(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 6, 3], deep_stem=True, avg_down=True, **kwargs)
+
+def resnet101_d(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 23, 3], deep_stem=True, avg_down=True, **kwargs)
+
+def resnet152_d(**kwargs):
+    return ResNet(Bottleneck, [3, 8, 36, 3], deep_stem=True, avg_down=True, **kwargs)
 
 def resnet50_w2x(**kwargs):
     return ResNet(Bottleneck, [3, 4, 6, 3], width=2, **kwargs)
@@ -188,24 +198,8 @@ def resnet50_w2x(**kwargs):
 def resnet50_16s(**kwargs):
     return ResNet(Bottleneck, [3, 4, 6, 3], layer4_dilation=2, **kwargs)
 
-def resnet50_d(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 6, 3], deep_stem=True, avg_down=True, **kwargs)
-
-def resnet101(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
-
-def resnet101_d(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 23, 3], deep_stem=True, avg_down=True, **kwargs)
-
 def resnext101_32x8d(**kwargs):
     return ResNet(Bottleneck, [3, 4, 23, 3], groups=32, width_per_group=8, **kwargs)
 
-def resnet152(**kwargs):
-    return ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
-
-def resnet152_d(**kwargs):
-    return ResNet(Bottleneck, [3, 8, 36, 3], deep_stem=True, avg_down=True, **kwargs)
-
 def resnext152_32x8d(**kwargs):
     return ResNet(Bottleneck, [3, 8, 36, 3], groups=32, width_per_group=8, **kwargs)
-
